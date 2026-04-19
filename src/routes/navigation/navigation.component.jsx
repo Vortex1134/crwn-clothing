8e4aa25d0ccbc450b919dfe542163a6583ff6906
Vortex1134 <bonjovifan855@aol.com>
2026-04-19 10:53:59 -0400

    added navigation bar

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,30 @@
+import { Fragment } from 'react'
+import { Outlet, Link } from 'react-router'
+
+import CrownLogo from '../../assets/crown.svg?react'
+
+import './navigation.styles.scss'
+
+const Navigation = () => {
+	return (
+		<Fragment>
+			<div className='navigation'>
+				<Link
+					className='logo-container'
+					to='/'>
+					<CrownLogo className='logo' />
+				</Link>
+				<div className='nav-links-container'>
+					<Link
+						className='nav-link'
+						to='/shop'>
+						SHOP
+					</Link>
+				</div>
+			</div>
+			<Outlet />
+		</Fragment>
+	)
+}
+
+export default Navigation
