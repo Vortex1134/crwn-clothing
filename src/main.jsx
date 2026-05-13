fcc56866ebea7fb872316562c874af3010030c09
Vortex1134 <bonjovifan855@aol.com>
2026-05-13 12:08:58 -0400

    added category preview to shop page

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,20 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import './index.scss'
 import App from './App.jsx'
+
 import { UserProvider } from './contexts/user.context'
-import { ProductsProvider } from './contexts/products.context'
+import { CategoriesProvider } from './contexts/categories.context'
 import { CartProvider } from './contexts/cart.context.jsx'
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
 		<BrowserRouter>
 			<UserProvider>
-				<ProductsProvider>
+				<CategoriesProvider>
 					<CartProvider>
 						<App />
 					</CartProvider>
-				</ProductsProvider>
+				</CategoriesProvider>
 			</UserProvider>
 		</BrowserRouter>
 	</StrictMode>,
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,21 +1,19 @@
 import './shop.styles.scss'
 
 import { useContext } from 'react'
-import { ProductsContext } from '../../contexts/products.context'
+import { CategoriesContext } from '../../contexts/categories.context'
 
-import ProductCard from '../../components/product-card/product-card.component'
+import CategoryPreview from '../../components/category-preview/category-preview.component'
 
 const Shop = () => {
-	const { products } = useContext(ProductsContext)
+	const { categoriesMap } = useContext(CategoriesContext)
 
 	return (
 		<div className='shop-container'>
-			{products.map((product) => (
-				<ProductCard
-					key={product.id}
-					product={product}
-				/>
-			))}
+			{Object.keys(categoriesMap).map((title) => {
+				const products = categoriesMap[title]
+				return <CategoryPreview key={title} title={title} products={products} />
+			})}
 		</div>
 	)
 }
