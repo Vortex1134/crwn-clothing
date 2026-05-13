9097d9d8d979ffcf7b1e7d8d1f7f9a9ed2f0e52a
Vortex1134 <bonjovifan855@aol.com>
2026-05-13 14:20:45 -0400

    added routing for the different shopping categories

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,12 +1,18 @@
 import './category-preview.styles.scss'
 
+import { Link } from 'react-router'
+
 import ProductCard from '../product-card/product-card.component'
 
 const CategoryPreview = ({ title, products }) => {
 	return (
 		<div className='category-preview-container'>
 			<h2>
-				<span className='title'>{title.toUpperCase()}</span>
+				<Link
+					to={title}
+					className='title'>
+					{title.toUpperCase()}
+				</Link>
 			</h2>
 			<div className='preview'>
 				{products
---
+++ src/routes/category/category.component.jsx
@@ -0,0 +1,37 @@
+import './category.styles.scss'
+
+import { useState, useEffect } from 'react'
+
+import { useContext } from 'react'
+import { CategoriesContext } from '../../contexts/categories.context'
+
+import { useParams } from 'react-router'
+
+import ProductCard from '../../components/product-card/product-card.component'
+
+const Category = () => {
+	const { category } = useParams()
+	const { categoriesMap } = useContext(CategoriesContext)
+	const [products, setProducts] = useState([])
+
+	useEffect(() => {
+		setProducts(categoriesMap[category])
+	}, [category, categoriesMap])
+
+	return (
+		<>
+			<h2 className='category-title'>{category}</h2>
+			<div className='category-container'>
+				{products &&
+					products.map((product) => (
+						<ProductCard
+							key={product.id}
+							product={product}
+						/>
+					))}
+			</div>
+		</>
+	)
+}
+
+export default Category
